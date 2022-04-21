51305f8e5fdd1997dbaaf373bb76cd346d4e0243
jeguren00 <jeguren00@gmail.com>
2022-04-21 18:29:58 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+const index = require('./routes/index');
+const path = __dirname + '/views/';
+
+const Path = require('path');
+
+app.set('view engine', 'pug');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path));
+
+app.use('/css', express.static(Path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(Path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+
+
+app.use('/', index);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+mongoose.connect(
+  `mongodb://root:pass12345@localhost:27017/bookTalk?authSource=admin`,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  (err, res) => {
+    if (err) console.log(`ERROR: connecting to Database.  ${err}`);
+    else console.log(`Database Online: ${process.env.MONGO_DB}`);
+  }
+);
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+/*
+  /routes/index.js
+*/
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+
+
+//Middleware para mostrar datos del request
+router.use (function (req,res,next) {
+  console.log('/' + req.method);
+  next();
+});
+
+//Analiza la ruta y el protocolo y reacciona en consecuencia
+router.get('/',function(req,res){
+  res.sendFile(path.resolve('views/test.html'));
+});
+
+module.exports = router;
