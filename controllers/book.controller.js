896d31a259895bff4b5f38f1547aa9bb54e7e20a
jeguren00 <jeguren00@gmail.com>
2022-04-26 20:53:57 +0200

    intentar acabar input doble llibres

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -3,16 +3,19 @@ const Author = require('../models/author.model');
 const Genre = require('../models/genre.model');
 
 
-exports.save = (req, res, next) => { 
+exports.save = async (req, res, next) => { 
+    //get author name and search it to grab the model
     let getAuthor = req.body.author;
-    let author = Author.model.findOne({completeName: getAuthor});
-    //mirar de ficar await, que agafa nomes la cadena de text, no la query
+    let author = await Author.model.findOne({completeName: getAuthor});
+
+    //get genre name and search it to grab the model
     let getGenre = req.body.genre;
-    let genre = Genre.model.findOne({name: getGenre});
+    let genre = await Genre.model.findOne({name: getGenre});
 
     console.log(author + " " + genre);
 
-    let book = new Book({bookName: req.body.bookName, author: req.body.author, genre: req.body.genre});
+    //save the book
+    let book = new Book({bookName: req.body.bookName, author: author, genre: genre});
     console.log(book);
     book.save();
     res.redirect("/");
