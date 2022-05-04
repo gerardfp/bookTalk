1a4d5308cd3e26868f61fe07b6f5000b4c1de2ab
jeguren00 <jeguren00@gmail.com>
2022-05-04 18:32:42 +0200

    control errors review, que ja estigui feta, nomes controller

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -4,10 +4,12 @@ var Book = require('../models/book.model.js');
 
 exports.save = async (req, res, next) => {
     let bookid = await Book.findOne({bookName: req.body.booktitle});
-    console.log(req.body);
-    let review = new Review.model({reviewTitle: req.body.reviewTitle.toLowerCase(), reviewScore: req.body.score , reviewText: req.body.review , bookImage: "", numberOfLikes: 0, bookId: bookid._id, username: req.body.username});
-    console.log(review);
-    review.save();
+    let testSearchReview = await Review.model.findOne({bookId: bookid._id, username: req.body.username});
+    if (testSearchReview == undefined) {
+        let review = new Review.model({reviewTitle: req.body.reviewTitle.toLowerCase(), reviewScore: req.body.score , reviewText: req.body.review , bookImage: "", numberOfLikes: 0, bookId: bookid._id, username: req.body.username});
+        console.log(review);
+        review.save();    
+    }
     res.redirect("/");
 }
 
@@ -20,13 +22,13 @@ exports.listMadeByUser = async (req, res, next) => {
     next();
 }
 
-exports.list  = async (req, res, next) => {
+exports.list = async (req, res, next) => {
     req.allReviewList = await Review.model.find();
     console.log(req.allReviewList);
     next();
 }
 
-exports.oneReview  = async (req, res, next) => {
+exports.oneReview = async (req, res, next) => {
     req.theReview = await Review.model.findOne({_id:req.params.idReview});
     req.theBook = await Book.findOne({ _id: req.theReview.bookId });
     console.log(req.theReview);
