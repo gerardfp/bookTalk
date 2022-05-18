6b25e03c199657e3fc80da4c9ec0bb795a395a85
jeguren00 <jordi@jordidebian.home>
2022-05-18 09:49:23 +0200

    variables changed for personal usage

--- app.js
+++ app.js
@@ -23,16 +23,16 @@ app.use('/public', express.static(Path.join(__dirname, '/public/profilePictures'
 app.use('/', index);
 
 //First option is for runing at home, second one is used in a deployment
-// app.listen(3000, function () {
-//   console.log(`Example app listening on port ${process.env.PORT}!`);
-// });
-app.listen(process.env.PORT, function () {
+app.listen(3000, function () {
   console.log(`Example app listening on port ${process.env.PORT}!`);
 });
+// app.listen(process.env.PORT, function () {
+//   console.log(`Example app listening on port ${process.env.PORT}!`);
+// });
 
 mongoose.connect(
   process.env.MONGO_URL,
-  //`mongodb://root:pass12345@localhost:27017/bookTalk?authSource=admin`,
+  `mongodb://root:pass12345@localhost:27017/bookTalk?authSource=admin`,
   { useUnifiedTopology: true, useNewUrlParser: true },
   (err, res) => {
     if (err) console.log(`ERROR: connecting to Database.  ${err}`);
